2b586b3431c97e9c5fc418d550b15a2d0109a5d3
measiit <nahssy248@gmail.com>
2026-04-02 16:24:03 +0530

    Facebook Social Graph Connections Analysis.py

--- Facebook Social Graph Connections Analysis.py
+++ Facebook Social Graph Connections Analysis.py
@@ -6,9 +6,7 @@ import matplotlib.pyplot as plt
 
 G = nx.from_pandas_edgelist(
     pd.read_csv("http://snap.stanford.edu/data/facebook_combined.txt.gz",
-                compression='gzip', sep=' ', names=['n1', 'n2']),
-    'n1', 'n2'
-)
+                compression='gzip', sep=' ', names=['n1', 'n2']),'n1', 'n2')
 
 print("Nodes:", G.number_of_nodes())
 print("Edges:", G.number_of_edges())
