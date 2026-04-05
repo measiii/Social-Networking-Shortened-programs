ca0bad762f106895567da37ddc0cea5d01a467a1
JANDAAAA <nahssy248@gmail.com>
2026-04-05 13:09:04 +0530

    Facebook Social Graph API Exploration.py

--- Facebook Social Graph API Exploration.py
+++ Facebook Social Graph API Exploration.py
@@ -2,15 +2,12 @@
 
 import requests
 
-# Paste your access token here
 access_token = "EAAR0ZAhPwkuEBRDFIvojUZCZBAwsOtUGIfNTZBK5DCZAShuczZAnTqbMmSdu114x4kAkrqoH4e90snkctj7jP7iRk7mMlTk0kSY8fdIr9YZAupvSDZBCW23YMR3mRkCNsZADZBoKNquDk5kO4sNaRbfVZCV3NwG3YjiYQeQGB4FQW9Hb6ZC533zjpc8ZCozO2exEdLZBr6"
 
-# Get basic profile data
 url = "https://graph.facebook.com/me?access_token=" + access_token
 
 response = requests.get(url)
 data = response.json()
 
-# Print output
 print("Name:", data["name"])
 print("ID:", data["id"])
